b599eae6da4fda8c6b9e59e4c21f9f41082afd67
Chiranjivi Khatri <143805001+Chiran36@users.noreply.github.com>
2025-12-18 12:00:19 +0545

    deployment time

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+   // <-- THIS MUST match your repo name
   plugins: [react()],
+  base: "/todo-react",
 })
